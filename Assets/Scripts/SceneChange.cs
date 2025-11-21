dfe204c373e63cd40448c8fcd36e6f4fdf92b7f7
BlooHoodDood <06emilhed@gmail.com>
2025-11-21 14:04:44 +0100

    Title screen with button
    Added a new scene with text (TMP) and a button that when click switches scenes

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChange : MonoBehaviour
+{
+    public Button yourButton;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene (sceneName:"SampleScene");
+    }
+}
